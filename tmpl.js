422a70e36a53bd6a92dfe986e769ab02935eee19
Wayne <dev.wayne.ix@gmail.com>
2014-10-16 11:35:14 +0800

    init

---
+++ tmpl.js
@@ -0,0 +1,14 @@
+
+
+var Tmpl=function(){
+
+}
+
+Tmpl.prototype.render(template, data) {
+  return template.replace(/\{([\w\.]*)\}/g, function(str, key) {
+    var keys = key.split("."), v = data[keys.shift()];
+    for (var i = 0, l = keys.length; i < l; i++) v = v[keys[i]];
+    return (typeof v !== "undefined" && v !== null) ? v : "";
+  });
+}
+
