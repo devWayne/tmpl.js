f154171a0311d316e385f2ecf7210b2178a3c96a
Wayne <dev.wayne.ix@gmail.com>
2014-12-16 13:54:23 +0800

    adddemo

--- tmpl.js
+++ tmpl.js
@@ -4,11 +4,15 @@ var Tmpl=function(){
 
 }
 
-Tmpl.prototype.render(template, data) {
-  return template.replace(/\{([\w\.]*)\}/g, function(str, key) {
+var p=Tmpl.prototype;
+
+
+p.render=function(template, data) {
+  return template.replace(/\{([\w\.]*)\}/g,baseReplace);
+}
+
+function baseReplace(str,key) {
     var keys = key.split("."), v = data[keys.shift()];
     for (var i = 0, l = keys.length; i < l; i++) v = v[keys[i]];
     return (typeof v !== "undefined" && v !== null) ? v : "";
-  });
 }
-
